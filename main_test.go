22ad3210fcdd795d2cb733115ca3f78d64cf4a09
dansyuqri <justanormalme@gmail.com>
2020-08-25 15:09:12 +0800

    Make createTemp flexible in the number of files it can create

--- main_test.go
+++ main_test.go
@@ -5,26 +5,34 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
-func createTemp() (tempDir string) {
+func createTemp(exts []string) (tempDir string) {
 	tempContent := []byte("tempContent")
 	tempDir, err := ioutil.TempDir("", "tempDir")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	tmpFile := filepath.Join(tempDir, "tmpfile")
-	if err := ioutil.WriteFile(tmpFile, tempContent, 0666); err != nil {
-		log.Fatal(err)
+	for _, ext := range exts {
+		var strBuilder strings.Builder
+		strBuilder.WriteString("tmpfile")
+		strBuilder.WriteString(ext)
+		tmpFile := filepath.Join(tempDir, strBuilder.String())
+		if err := ioutil.WriteFile(tmpFile, tempContent, 0666); err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	return tempDir
 }
 
 func TestCreateTemp(t *testing.T) {
-	tempDir := createTemp()
+	exts := []string{".py", ".txt", ".java"}
+
+	tempDir := createTemp(exts)
 	defer os.RemoveAll(tempDir)
 	fileInfo, err := ioutil.ReadDir(tempDir)
 
@@ -32,7 +40,7 @@ func TestCreateTemp(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	if len(fileInfo) != 1 {
-		t.Errorf("want 1, got %v", len(fileInfo))
+	if len(fileInfo) != len(exts) {
+		t.Errorf("want %v, got %v", len(exts), len(fileInfo))
 	}
 }
